cc34acd2758078b2624d42e60c1411a098b04b15
pcochoco <121675930+pcochoco@users.noreply.github.com>
2026-03-03 17:52:55 +0900

    [feat] 수정 검색 조건에 대한 repo 변경

--- qiin-be/src/main/java/com/beyond/qiin/domain/booking/repository/querydsl/AppliedReservationsQueryRepository.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/booking/repository/querydsl/AppliedReservationsQueryRepository.java
@@ -2,8 +2,11 @@ package com.beyond.qiin.domain.booking.repository.querydsl;
 
 import com.beyond.qiin.domain.booking.dto.reservation.request.search_condition.GetAppliedReservationSearchCondition;
 import com.beyond.qiin.domain.booking.dto.reservation.response.raw.RawAppliedReservationResponseDto;
+import com.beyond.qiin.domain.booking.enums.ReservationStatus;
+import com.beyond.qiin.domain.booking.vo.DateRange;
 import java.util.List;
 
 public interface AppliedReservationsQueryRepository {
-    List<RawAppliedReservationResponseDto> search(GetAppliedReservationSearchCondition condition);
+    List<RawAppliedReservationResponseDto> search(
+            GetAppliedReservationSearchCondition condition, DateRange range, ReservationStatus reservationStatus);
 }
--- qiin-be/src/main/java/com/beyond/qiin/domain/booking/repository/querydsl/AppliedReservationsQueryRepositoryImpl.java
+++ qiin-be/src/main/java/com/beyond/qiin/domain/booking/repository/querydsl/AppliedReservationsQueryRepositoryImpl.java
@@ -3,18 +3,15 @@ package com.beyond.qiin.domain.booking.repository.querydsl;
 import com.beyond.qiin.domain.booking.dto.reservation.request.search_condition.GetAppliedReservationSearchCondition;
 import com.beyond.qiin.domain.booking.dto.reservation.response.raw.RawAppliedReservationResponseDto;
 import com.beyond.qiin.domain.booking.entity.QReservation;
+import com.beyond.qiin.domain.booking.enums.ReservationStatus;
+import com.beyond.qiin.domain.booking.vo.DateRange;
 import com.beyond.qiin.domain.iam.entity.QUser;
 import com.beyond.qiin.domain.inventory.entity.QAsset;
 import com.beyond.qiin.domain.inventory.entity.QAssetClosure;
 import com.beyond.qiin.domain.inventory.entity.QCategory;
-import com.beyond.qiin.domain.inventory.enums.AssetStatus;
-import com.beyond.qiin.domain.inventory.enums.AssetType;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -33,19 +30,25 @@ public class AppliedReservationsQueryRepositoryImpl implements AppliedReservatio
     private static final QUser respondent = new QUser("respondent");
 
     @Override
-    public List<RawAppliedReservationResponseDto> search(GetAppliedReservationSearchCondition condition) {
+    public List<RawAppliedReservationResponseDto> search(
+            GetAppliedReservationSearchCondition condition, DateRange range, ReservationStatus reservationStatus) {
 
         BooleanBuilder builder = new BooleanBuilder();
         builder.and(reservation.isApplied.eq(true)); // 신청된 경우
 
         // 날짜(Instant)
-        if (condition.getDate() != null) {
-            LocalDate date = condition.getDate();
+        if (range.getStartDay() != null) {
+            builder.and(reservation.startAt.goe(range.getStartDay()));
+        }
 
-            Instant start = date.atStartOfDay(ZoneId.of("Asia/Seoul")).toInstant();
-            Instant end = date.plusDays(1).atStartOfDay(ZoneId.of("Asia/Seoul")).toInstant();
+        if (range.getEndDay() != null) {
+            builder.and(reservation.startAt.lt(range.getEndDay()));
+        }
 
-            builder.and(reservation.startAt.between(start, end));
+        // reservation status
+        if (reservationStatus != null) {
+
+            builder.and(reservation.status.eq(reservationStatus.getCode()));
         }
 
         // 신청자 이름 검색
@@ -53,63 +56,14 @@ public class AppliedReservationsQueryRepositoryImpl implements AppliedReservatio
             builder.and(applicant.userName.containsIgnoreCase(condition.getApplicantName()));
         }
 
-        // 승인자 이름 검색
-        if (condition.getRespondentName() != null) {
-            builder.and(respondent.userName.containsIgnoreCase(condition.getRespondentName()));
-        }
-
-        // 승인 여부
-        if (condition.getIsApproved() != null) {
-            boolean approved = Boolean.parseBoolean(condition.getIsApproved());
-            builder.and(reservation.isApproved.eq(approved));
-        }
-
         // 자원명
         if (condition.getAssetName() != null) {
             builder.and(asset.name.containsIgnoreCase(condition.getAssetName()));
         }
 
-        // 자원 상태 (assetStatus) 필터링
-        if (condition.getAssetStatus() != null) {
-            String raw = condition.getAssetStatus().trim();
-
-            try {
-                AssetStatus statusEnum = AssetStatus.valueOf(raw.toUpperCase());
-
-                builder.and(asset.status.eq(statusEnum.getCode()));
-
-            } catch (IllegalArgumentException ignored) {
-            }
-        }
-
+        // category id
         if (condition.getCategoryId() != null) {
             builder.and(asset.category.id.eq(condition.getCategoryId()));
-        }
-
-        // 자원 유형(int) - 변환된 값 사용
-        if (condition.getAssetType() != null) {
-            String raw = condition.getAssetType().trim();
-
-            try {
-                AssetType statusEnum = AssetType.valueOf(raw.toUpperCase());
-
-                builder.and(asset.type.eq(statusEnum.getCode()));
-
-            } catch (IllegalArgumentException ignored) {
-            }
-        }
-
-        BooleanBuilder closureFilter = new BooleanBuilder();
-        boolean needsClosureJoin = false;
-
-        if (condition.getLayerOne() != null) {
-            needsClosureJoin = true;
-            builder.and(closure.assetClosureId.ancestorId.eq(Long.valueOf(condition.getLayerOne())))
-                    .and(closure.depth.gt(0)); // 자기 자신 제외
-        } else if (condition.getLayerZero() != null) {
-            needsClosureJoin = true;
-            builder.and(closure.assetClosureId.ancestorId.eq(Long.valueOf(condition.getLayerZero())))
-                    .and(closure.depth.gt(0)); // 자기 자신 제외
         }
 
         // 조회
@@ -131,10 +85,6 @@ public class AppliedReservationsQueryRepositoryImpl implements AppliedReservatio
                 .leftJoin(asset.category, category)
                 .leftJoin(reservation.applicant, applicant)
                 .leftJoin(reservation.respondent, respondent);
-
-        if (needsClosureJoin) {
-            queryBuilder.leftJoin(closure).on(closure.assetClosureId.descendantId.eq(asset.id));
-        }
 
         return queryBuilder.where(builder).orderBy(reservation.id.desc()).fetch();
     }
