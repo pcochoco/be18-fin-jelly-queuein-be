0275e7e9cab356e986c13c64f4ae5f4a7b906dad
pcochoco <121675930+pcochoco@users.noreply.github.com>
2026-05-12 17:57:33 +0900

    [feat] actuator 경로에 대한 인가 설정

--- qiin-be/src/main/java/com/beyond/qiin/security/config/SecurityConfig.java
+++ qiin-be/src/main/java/com/beyond/qiin/security/config/SecurityConfig.java
@@ -43,12 +43,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(final HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http.csrf(AbstractHttpConfigurer::disable) // jwt이므로 disable
                 .cors(Customizer.withDefaults())
                 .sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**")
+                        .permitAll()
+                        .requestMatchers(PUBLIC)
                         .permitAll()
                         .requestMatchers(SSE)
                         .permitAll()
